9cf902c7d406670d07f4f1ee7c309cc5ae3dd119
LucianoFP <lucianofp94@gmail.com>
2024-12-10 17:41:12 +0100

    1_backup_data.sql

--- sql/1_backup_data.sql
+++ sql/1_backup_data.sql
@@ -1,5 +1,5 @@
 -- Duplico la base de datos, para tener un respaldo. Crearé una que se llame original.
 
 CREATE DATABASE original;
--- Aquí se deben incluir los pasos necesarios para clonar la base de datos.
+
 Puede hacerse via codigo, pero decidí insertar el archivo manualmente. Para eso, cree un esquema llamado original, y luego inserté la tabla con el "Table Data import Wizard".

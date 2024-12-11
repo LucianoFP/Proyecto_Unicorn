7498b89f0afe23fd3f2e54d2976074763716bd8f
LucianoFP <lucianofp94@gmail.com>
2024-12-11 16:55:48 +0100

    1_ Create_y_backup_data.sql

---
+++ sql/1_ Create_y_backup_data.sql
@@ -0,0 +1,5 @@
+1. Crear el esquema e insertar la tabla.
+En esta parte, cree el esquema de forma manual, con la herramienta que nos brinda my sql
+
+2. Crear un Respaldo de la Base de Datos (Manual)
+Descripción: En este paso, decidí duplicar la base de datos manualmente en lugar de hacerlo a través de código SQL. Para ello, creé un nuevo esquema llamado original que servirá como respaldo de la base de datos original. 
--- sql/1_backup_data.sql
+++
@@ -1,8 +0,0 @@
--- Duplico la base de datos para tener un respaldo. 
--- Crearé una copia de la base de datos original llamada "original" para realizar cambios sin afectar los datos originales.
--- Este paso asegura que en caso de un error, podamos restaurar la base de datos original sin perder información.
-
-
-CREATE DATABASE original;
-
-Puede hacerse via codigo, pero decidí insertar el archivo manualmente. Para eso, cree un esquema llamado original, y luego inserté la tabla con el "Table Data import Wizard".

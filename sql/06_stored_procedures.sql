d62f26d8e0c518b915cab5729f6ca506dde98069
LucianoFP <lucianofp94@gmail.com>
2024-12-24 09:42:37 +0100

    06_stored_procedures.sql

--- sql/06_stored_procedures.sql
+++ sql/06_stored_procedures.sql
@@ -20,3 +20,50 @@ BEGIN
 END //
 
 DELIMITER ;
+
+Promedio de Uso de Redes Sociales por Género:** Calcula el tiempo promedio en redes sociales desglosado por género.
+
+DELIMITER //
+
+CREATE PROCEDURE calculate_social_media_avg_by_gender()
+BEGIN
+    SELECT 
+        gender AS 'Género',
+        ROUND(AVG(hours_on_social_media), 2) AS 'Promedio de Uso de Redes Sociales (horas)'
+    FROM 
+        student_data
+    GROUP BY 
+        gender
+    ORDER BY 
+        gender;
+END //
+
+DELIMITER ;
+
+Detección de Estudiantes en Riesgo Académico:** Identifica estudiantes con bajo rendimiento y clasifica su nivel de riesgo según el impacto de las redes sociales.
+
+    DELIMITER $$
+
+CREATE PROCEDURE identify_students_at_risk()
+BEGIN
+    SELECT 
+        student_id AS 'ID Estudiante',
+        name AS 'Nombre',
+        academic_performance AS 'Rendimiento Académico',
+        hours_on_social_media AS 'Horas en Redes Sociales',
+        CASE 
+            WHEN academic_performance < 50 AND hours_on_social_media > 5 THEN 'Alto Riesgo'
+            WHEN academic_performance < 70 AND hours_on_social_media > 4 THEN 'Moderado'
+            ELSE 'Bajo'
+        END AS 'Nivel de Riesgo'
+    FROM 
+        student_data
+    WHERE 
+        academic_performance < 70
+    ORDER BY 
+        academic_performance ASC, hours_on_social_media DESC;
+END $$
+
+DELIMITER ;
+
+
